9dde92b01738f364c9ee59713f1688bd6f22a675
Anish Gondhi <159081051+AnishGondhiDC@users.noreply.github.com>
2024-06-19 17:00:44 -0300

    call back functions

--- Practicing JS/main.js
+++ Practicing JS/main.js
@@ -1,6 +1,18 @@
-function findsum(a, b){ // takes some parameters
-    return a+b; // does computation tasks and returns it
+// callback functions
+
+function sum (num1, num2, fnToCall){
+    let result = num1 + num2;
+    fnToCall(result);
 }
-const value = findsum (1,2)
-console.log(value)
+
+function displayResult(data){
+    console.log("result of the sum is : "+data);
+}
+
+function displayResultPassive(data){
+    console.log("Sum's result is : "+data);
+}
+
+const ans = sum(1,2,displayResult);
+const ans1 = sum(1,2,displayResultPassive);
 
