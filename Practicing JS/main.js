f49386e3620553247fde3a90066d4673412f534b
Anish Gondhi <159081051+AnishGondhiDC@users.noreply.github.com>
2024-06-19 15:57:28 -0300

    different types

--- Practicing JS/main.js
+++ Practicing JS/main.js
@@ -1,4 +1,4 @@
-//defining variables
-let a = 1;
-a = 2;
-console.log(a);
+let firstName = "Anish"
+let age = 22;
+let isMarried = false;
+console.log("this persons name is "+firstName+" and their age is "+age);
