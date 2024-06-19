e03de5cc953a740b42064eef6697555687b98724
Anish Gondhi <159081051+AnishGondhiDC@users.noreply.github.com>
2024-06-19 16:00:51 -0300

    boolean and if/else

--- Practicing JS/main.js
+++ Practicing JS/main.js
@@ -1,4 +1,10 @@
 let firstName = "Anish"
 let age = 22;
-let isMarried = false;
-console.log("this persons name is "+firstName+" and their age is "+age);
+let isMarried = true;
+
+if(isMarried){
+    console.log(firstName+ " is Married");
+}
+else{
+    console.log(firstName+" is not Married");
+}
