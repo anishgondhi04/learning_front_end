7e9c64f4b3628b289fa1b7112fa4f69fba410057
Anish Gondhi <159081051+AnishGondhiDC@users.noreply.github.com>
2024-06-30 19:11:20 -0300

    callbacks 1

--- Practicing JS/main.js
+++ Practicing JS/main.js
@@ -1,8 +1,17 @@
-//reading files in javascript
-const fs = require("fs");
-
-fs.readFile("a.txt","utf8",function(err,data){
-    console.log(data);
-})
-console.log("hi there");
- 
+// understandiong callbacks functions
+function square(n) {
+    return n * n;
+  }
+  
+  function cube(n) {
+    return n * n * n;
+  }
+  
+  function sumOfSquares(a, b) {
+    let square1 = square(a);
+    let square2 = square(b);
+    return square1 + square2;
+  }
+  
+  let ans = sumOfSquares(1, 2);
+  console.log(ans);
