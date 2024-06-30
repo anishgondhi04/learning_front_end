1ca5a6fde435cdf16063797783fb80fe7b5e06b4
Anish Gondhi <159081051+AnishGondhiDC@users.noreply.github.com>
2024-06-30 19:15:54 -0300

    callback

--- Practicing JS/main.js
+++ Practicing JS/main.js
@@ -1,17 +1,23 @@
-// understandiong callbacks functions
+// implementing callback
+// trying to avoid repetitions
 function square(n) {
     return n * n;
-  }
-  
-  function cube(n) {
+}
+
+function cube(n) {
     return n * n * n;
-  }
-  
-  function sumOfSquares(a, b) {
-    let square1 = square(a);
-    let square2 = square(b);
-    return square1 + square2;
-  }
-  
-  let ans = sumOfSquares(1, 2);
-  console.log(ans);
+}
+
+function quad(n) {
+    return n * n * n * n;
+}
+
+function sumOfSomething(a, b, fn) {
+    let first = fn(a);
+    let second = fn(b);
+    return first + second;
+
+}
+
+let ans = sumOfSomething(1, 2, cube);
+console.log(ans);
