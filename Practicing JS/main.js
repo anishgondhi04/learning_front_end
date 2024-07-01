70d8e1c6c7c8c04e6dcf782c8d152c731cecdac9
Anish Gondhi <159081051+AnishGondhiDC@users.noreply.github.com>
2024-07-01 15:39:38 -0300

    formatted

--- Practicing JS/main.js
+++ Practicing JS/main.js
@@ -1,12 +1,12 @@
 // syntax of promises
 
-function promisifiedMyOwnSetTimeout(duration){
-    const p = new Promise(function(resolve){
+function promisifiedMyOwnSetTimeout(duration) {
+    const p = new Promise(function (resolve) {
         setTimeout(resolve, duration)
     });
     return p;
- }
- const ans = promisifiedMyOwnSetTimeout(1000);
-ans.then(function(){
+}
+const ans = promisifiedMyOwnSetTimeout(1000);
+ans.then(function () {
     console.log("timeout is done")
 })
