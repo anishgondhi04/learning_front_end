cc32e041636f54aba2b481049844c1b124ec2aa5
Anish Gondhi <159081051+AnishGondhiDC@users.noreply.github.com>
2024-07-17 18:31:16 -0300

    body parser

--- week 2.2/index.js
+++ week 2.2/index.js
@@ -1,18 +1,21 @@
 
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
 
 const port = 3000
 
-//create a todo app that lets users store todos on the server
+app.use(bodyParser.json());
+
+// create a todo app that lets users store todos on the server
 // try to create a http server from scratch in c
 // create an http server in rust uising actix-web
 // create an http server in goland using the gurrila framework
 // spring boot java
-app.get('/',function(req, res){
+app.post('/', function (req, res) {
+    console.log(req.body)
     res.send('Hello World!')
 })
-app.listen(port,function(){
+app.listen(port, function () {
     console.log('Example app listening on port ${port}')
 })
