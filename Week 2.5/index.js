5c7d629307be930ea7fe6a2e0007a4f926592414
Anish Gondhi <159081051+AnishGondhiDC@users.noreply.github.com>
2024-07-19 18:32:13 -0300

    adding post and put endpoint

--- Week 2.5/index.js
+++ Week 2.5/index.js
@@ -2,24 +2,24 @@ const express = require("express")
 const app = express();
 
 var users = [{
-    name:"john",
+    name: "john",
     kidneys: [{
         healthy: false
     }]
 
 }];
 
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
     // write logic
     const johnkidneys = users[0].kidneys;
     const numberOfKidneys = johnkidneys.length;
 
     let numbeOfHealthyKidneys = 0;
     for (let index = 0; index < numberOfKidneys; index++) {
-        if(johnkidneys[index].healthy){
+        if (johnkidneys[index].healthy) {
             numbeOfHealthyKidneys += 1;
         }
-        
+
     }
     const numbeOfUnHealthyKidneys = numberOfKidneys - numbeOfHealthyKidneys;
 
@@ -28,6 +28,24 @@ app.get("/",function(req,res){
         numbeOfHealthyKidneys,
         numbeOfUnHealthyKidneys
     })
-});
+})
 
+app.use(express.json());
+
+app.post("/", function (req, res) {
+    const isHealthy = req.body.isHealthy;
+    users[0].kidneys.push({
+        healthy: isHealthy
+    })
+    res.json({
+        msg: "Done!"
+    })
+})
+
+app.put("/", function (req, res) {
+    for (let i = 0; i < users[0].kidneys.length; i++) {
+        users[0].kidneys[i].healthy = true;
+    }
+    res.json({})
+})
 app.listen(3000)
