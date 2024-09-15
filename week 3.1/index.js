f6f3430469c563efb938732fc9255536e31c065d
Anish Gondhi <159081051+AnishGondhiDC@users.noreply.github.com>
2024-09-14 21:21:13 -0300

    schema object

--- week 3.1/index.js
+++ week 3.1/index.js
@@ -2,7 +2,12 @@ const express = require("express");
 const zod = require("zod")
 
 const app = express();
-const schema = zod.array(zod.number())
+
+const schema = zod.object({
+    email: zod.string(),
+    password: zod.string(),
+    kidneys: zod.array(zod.number())
+})
 
 app.use(express.json())
 
